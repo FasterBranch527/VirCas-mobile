476227fa72212a2d2759d53b111ed562da718982
FB527 <art.glukhovtsev@gmail.com>
2026-08-27 22:14:21 +0500

    refactor: let app shell own persisted theme selection

--- app/src/main/java/com/vircas/mobile/MainActivity.kt
+++ app/src/main/java/com/vircas/mobile/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.vircas.mobile.ui.VirCasApp
-import com.vircas.mobile.ui.theme.VirCasTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { VirCasTheme { VirCasApp() } }
+        setContent { VirCasApp() }
     }
 }
